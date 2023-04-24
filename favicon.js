e0a6bb92a82d9e9e08b671227c64aba6003b2866
39l6 <130191016+39l6@users.noreply.github.com>
2023-04-24 14:32:37 -0600

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -2,20 +2,33 @@ function favico() {
     var canvas = document.createElement('canvas'),
         ctx,
         link = document.createElement('link'),
-        day = (new Date).getDate() + '';// en vez de toString
-    
-    if (canvas.getContext) {
-      canvas.height = canvas.width = 16;
-      ctx = canvas.getContext('2d');
-      ctx.drawImage(this, 0, 0);
-      ctx.font = 'bold 10px "helvetica", sans-serif';
-      ctx.fillStyle = '#F0EEDD';
-      if (day.length == 1) day = '0' + day;
-      ctx.fillText(day, 2, 12);
-      link.href = canvas.toDataURL('image/webp');
-      document.body.appendChild(link);
+        day = (new Date).getDate() + '',// en vez de toString
+       canvas.height = canvas.width = 16,
+       ctx = canvas.getContext('2d');
+
+       ctx.font = 'bold 10px "helvetica", sans-serif';
+       ctx.fillStyle = '#F0EEDD';
+       if (day.length == 1) day = '0' + day;
+       ctx.fillText(day, 2, 12);
+       link.href = canvas.toDataURL('image/webp');
+       
+     return document.body.appendChild(link);
     }
 }
-    
-    
+
+/*
+for(i = 0; i<color.length ; i++){
+    ctx.beginPath();
+    ctx.rect(0, 0, 8, 8);
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.rect(8, 8, 8, 8);
+    ctx.fillStyle = color[i];
+    ctx.fill(); 
+
+
+}   
+  */  
     //https://stackoverflow.com/questions/6296574/dynamic-favicon-using-image-manipulation-similar-to-gmail-adding-a-count
